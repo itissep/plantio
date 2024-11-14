86dab25f09bec3fde9be457d8f12d5b96cefcd5e
itissep <someonenewish@yandex.ru>
2024-11-15 00:12:02 +0300

    IOS-19 add generic api service

---
+++ Core/Core/Networking/Basic/NetworkingService.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Combine
+
+struct NetworkingService {
+    let session = URLSession.shared
+    
+    struct Response<T> {
+        let value: T
+        let response: URLResponse
+    }
+    
+    func run<T: Decodable>(_ request: URLRequest, _ decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<Response<T>, Error> {
+        return URLSession.shared
+            .dataTaskPublisher(for: request)
+            .tryMap { result -> Response<T> in
+                let value = try decoder.decode(T.self, from: result.data)
+                return Response(value: value, response: result.response)
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
