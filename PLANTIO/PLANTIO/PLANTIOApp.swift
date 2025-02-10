72b98377276e00a8979e442a1dd4d0aedad67de2
itissep <someonenewish@yandex.ru>
2025-02-10 20:28:51 +0300

    new main project

---
+++ PLANTIO/PLANTIO/PLANTIOApp.swift
@@ -0,0 +1,29 @@
+import Core
+import AuthModule
+import ProfileModule
+import SwiftUI
+
+@main
+struct PLANTIOApp: App {
+    private let serviceLocator: ServiceLocator
+    
+    init() {
+        let appServiceLocator = AppServiceLocator()
+        
+        appServiceLocator.register(MockAuthService() as AuthServiceProtocol)
+        appServiceLocator.register(MockProfileService() as ProfileServiceProtocol)
+        appServiceLocator.register(RegistrationValidationService())
+        
+        self.serviceLocator = appServiceLocator
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            AppCoordinatorView(
+                screenFactory: ScreenFactory(serviceLocator: serviceLocator),
+                coordinator: AppCoordinator(profileService: serviceLocator.resolve()!)
+            )
+            .preferredColorScheme(.light)
+        }
+    }
+}
