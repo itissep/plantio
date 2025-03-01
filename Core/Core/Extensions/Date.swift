5a1e54233ded953eed129146cc15b1aaff64d124
itissep <someonenewish@yandex.ru>
2025-03-01 22:13:55 +0300

    add date extensions

--- Core/Core/Extensions/Date.swift
+++ Core/Core/Extensions/Date.swift
@@ -27,4 +27,40 @@ public extension Date {
     func formattedAsDateTime() -> String {
         formatted(as: "HH:mm dd.MM.yyyy")
     }
+
+    init?(day: Int, month: Int, year: Int) {
+        var components = DateComponents()
+        components.day = day
+        components.month = month
+        components.year = year
+        
+        guard let date = Calendar.current.date(from: components) else {
+            return nil
+        }
+        
+        self = date
+    }
+    
+    static func isOneDay(_ firstDate: Date, secondDate: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.component(.day, from: firstDate) == calendar.component(.day, from: secondDate)
+        && calendar.component(.month, from: firstDate) == calendar.component(.month, from: secondDate)
+        && calendar.component(.year, from: firstDate) == calendar.component(.year, from: secondDate)
+    }
+    
+    static func date(day: Int, month: Int, year: Int) -> Date? {
+        var components = DateComponents()
+        components.day = day
+        components.month = month
+        components.year = year
+        
+        return Calendar.current.date(from: components)
+    }
+    
+    func isMonday() -> Bool {
+        let calendar = Calendar.current
+        let weekday = calendar.component(.weekday, from: self)
+        
+        return weekday == 2 // В Gregorian календаре понедельник — это 2, если неделя начинается с воскресенья.
+    }
 }
