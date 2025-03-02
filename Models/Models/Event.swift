38fb86e4105c71092f56fc9d5cf820325ccb1212
itissep <someonenewish@yandex.ru>
2025-03-02 18:22:46 +0300

    event model update

--- Models/Models/Event.swift
+++ Models/Models/Event.swift
@@ -3,25 +3,25 @@ import Foundation
 public struct Event {
     public let id: String
     public let note: String?
-    public let plant: Plant
+    public let plantId: String
     public let createdAt: Date
     
     
     public init(
         id: String,
         note: String?,
-        plant: Plant,
+        plantId: String,
         createdAt: Date
     ) {
         self.id = id
         self.note = note
-        self.plant = plant
+        self.plantId = plantId
         self.createdAt = createdAt
     }
 }
 
 public extension Event {
     static var mock: Event {
-        .init(id: UUID().uuidString, note: "some note", plant: Plant.mock, createdAt: .now)
+        .init(id: UUID().uuidString, note: "some note", plantId: Plant.mock.id, createdAt: .now)
     }
 }
